2812f2c09db87fdac3bb752e5e65f1d75d905666
Lynn Fleck <lynnfleck@me.com>
2016-09-24 15:00:06 -0400

    add welcome message with user email

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import firebase from '../../firebase.config.js';
 import NewIdea from './NewIdea.jsx';
 import IdeaList from './IdeaList.jsx';
 
 class Dashboard extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      userid: '',
+      useremail: '',
+    }
+    this.getUserInfo = this.getUserInfo.bind(this);
+  }
+  componentDidMount() {  // Only happens once.. on first load
+    this.getUserInfo();
+  }
+  getUserInfo() {
+  const user = firebase.auth().currentUser;
+    if (user) {
+      this.setState({
+        userid: user.uid,
+        useremail: user.email,
+      })
+    } else {
+      console.log('No user is logged in');
+    }
+  }
   render() {
     return (
       <div>
         <h1>This is the DASHBOARD page. This page IS protected</h1>
+        <p>Welcome {this.state.useremail}</p>
         <NewIdea
           id="123"
           mainIdea="This is the idea right here"
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,37 +1,11 @@
 import React, { Component } from 'react';
-import firebase from '../../firebase.config.js';
 
 
 class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userid: '',
-      useremail: '',
-    }
-    this.getUserInfo = this.getUserInfo.bind(this);
-  }
-  componentDidMount() {  // Only happens once.. on first load
-    this.getUserInfo();
-  }
-  getUserInfo() {
-  const user = firebase.auth().currentUser;
-    if (user) {
-      // console.log(user.email + ' is logged in');
-      // console.log(user.uid + ' is logged in');
-      this.setState({
-        userid: user.uid,
-        useremail: user.email
-      })
-    } else {
-      console.log('No user is logged in');
-    }
-  }
   render() {
     return (
       <div>
           <h1>I am the HOME PAGE - and feeling very unprotected</h1>
-          <p>Welcome {this.state.useremail}</p>
       </div>
       );
   };
